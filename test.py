52d115e6c3f6afe4d92613e2adb587149a6eaf71
Valando <43469380+Valando@users.noreply.github.com>
2024-09-20 16:32:33 +0200

    workng video

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import cv2
 
-img = cv2.imread("DJI_0001b.JPG")#picture
+img = cv2.imread("DJI_0001b.jpg")#picture
 gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #greyscale
 sift = cv2.SIFT_create() #do sift
 kp = sift.detect(gray,None) # create keypoints
 img=cv2.drawKeypoints(gray,kp,img) #add keypoints highlighted to image
 
 cv2.imwrite('sift_keypoints.jpg',img) #write the image to folder
-video = cv2.VideoCapture('DJI_0004_MOV')
+video = cv2.VideoCapture('DJI_0004.MOV')
+
 while video.isOpened():
     print("w")
 
     ret, frame = video.read()
     # if frame is read correctly ret is True
-    if ret:
+    if not ret:
         print("receive frame (stream end?). Exiting ...")
         break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
