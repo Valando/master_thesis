c8fd48106d14a30d8946f08f0f6e7dcd3fe774de
Valando <43469380+Valando@users.noreply.github.com>
2024-09-30 18:44:14 +0200

    test branch7

--- test.py
+++ test.py
@@ -2,11 +2,16 @@ import cv2
 import matplotlib.pyplot as plt
 
 # Load the image
-img = cv2.imread("DJI_0001b.jpg")
+img = cv2.imread("DJI_0001.jpg")
+img = img[1821:1920, 2635:2789]
+
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # ORB for both image and video to match descriptor types
 orb = cv2.ORB_create()
+sift = cv2.SIFT_create()
+brisk = cv2.BRISK_create()
+
 kp1, des1 = orb.detectAndCompute(gray, None)
 
 # Draw keypoints on the image
@@ -24,7 +29,13 @@ flann = cv2.FlannBasedMatcher(index_params, search_params)
 
 frame_counter = 0
 
-max_good_matches =0
+max_good_matches = 0
+
+# Initialize best matches-related variables
+best_kp2 = None
+best_matches_list = None
+best_matchesMask = None
+best_gray2 = None
 
 while video.isOpened():
     ret, frame = video.read()
@@ -48,11 +59,8 @@ while video.isOpened():
             if len(match) >= 2:  # Ensure there are at least two matches
                 m, n = match
                 if m.distance < 0.7 * n.distance:
-                    matchesMask.append([1,0])
+                    matchesMask.append([1, 0])
                     matches_list.append([m])
-
- 
-        #assert len(matches_list) == len(matchesMask), "matches_list and matchesMask must be of the same length."
 
         if len(matches_list) > max_good_matches:
             max_good_matches = len(matches_list)
@@ -64,25 +72,22 @@ while video.isOpened():
         # Reset frame counter
         frame_counter = 0
 
-        # Draw matches
-        draw_params = dict(matchColor=(0, 255, 0),
-                           singlePointColor=(255, 0, 0),
-                           matchesMask=best_matchesMask,
-                           flags=cv2.DrawMatchesFlags_DEFAULT)
-        img3 = cv2.drawMatchesKnn(gray, kp1, best_gray2, best_kp2, best_matches_list, None, **draw_params)
+        # Draw matches if best matches are found
+        if best_kp2 is not None:
+            draw_params = dict(matchColor=(0, 255, 0),
+                               singlePointColor=(255, 0, 0),
+                               matchesMask=best_matchesMask,
+                               flags=cv2.DrawMatchesFlags_DEFAULT)
+            img3 = cv2.drawMatchesKnn(gray, kp1, best_gray2, best_kp2, best_matches_list, None, **draw_params)
 
-        # Save and display the matches
-       # matches_list.append(img3)
-       # cv2.imshow("Matched Features", img3)
-
+        
     # Increment the frame counter
     frame_counter += 1
 
     # Exit if 'q' is pressed
     if cv2.waitKey(1) == ord('q'):
         break
-plt.imshow(img3),plt.show()
-
+plt.imshow(img3), plt.show()
 # Release the video capture and close windows
 video.release()
 cv2.waitKey(0)
